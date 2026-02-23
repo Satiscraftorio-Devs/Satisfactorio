a6028f552d9ae4a7a66e0eaab577fbe3a5f547c6
Strachy <strachy.pro@gmail.com>
2026-02-23 17:59:13 +0100

    Initial commit

--- src/world_generator/mod.rs
+++
@@ -1,6 +0,0 @@
-mod world_beta_setup;
-mod world_gen;
-mod chunks;
-
-pub use world_beta_setup::*;
-pub use world_gen::*;
--- src/world_generator_beta/mod.rs
+++
@@ -1,5 +0,0 @@
-mod perlin;
-mod mesh;
-
-pub use perlin::*;
-pub use mesh::*;
