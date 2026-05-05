4756ffbf0c9edb3fff67fd5992257f38d72ccd53
Strachy <strachy.pro@gmail.com>
2026-05-05 17:16:38 +0200

    Ajustements

--- client/src/engine/render/texture.rs
+++ client/src/engine/render/texture.rs
@@ -12,12 +12,19 @@ pub struct Texture {
     pub dimensions: (u32, u32),
 }
 
+#[repr(u8)]
 pub enum PipelineType {
     Opaque = 0,
     AlphaCutout = 1,
     Translucent = 2,
     Billboard = 3,
     UI = 4,
+}
+
+impl PipelineType {
+    pub const fn to_usize(self) -> usize {
+        self as usize
+    }
 }
 
 impl Texture {
